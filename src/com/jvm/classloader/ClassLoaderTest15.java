74577e39389efd9737fb2361ab24ac0523fcab6c
liuyongjie <Liu_1770599952>
2020-07-03 15:18:34 +0800

    类加载-- 加载器与类全限定名。

---
+++ src/com/jvm/classloader/ClassLoaderTest15.java
@@ -0,0 +1,7 @@
+package com.jvm.classloader;
+
+public class ClassLoaderTest15 {
+    public static void main(String[] args) {
+        System.out.println(ClassLoader.getSystemClassLoader());
+    }
+}
